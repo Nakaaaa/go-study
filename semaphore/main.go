e9416aad61c2b28c70b08b5bf2906e8b7c329eb4
Kazuya Nakamura <nkazuya6073@gmail.com>
2022-03-18 16:34:57 +0000

    Add semaphore

---
+++ semaphore/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"golang.org/x/sync/semaphore"
+)
+
+var s *semaphore.Weighted = semaphore.NewWeighted(1) // 同時に走らせるゴルーチンの数を指定
+
+func longProcess(ctx context.Context) {
+	// 他で待っているゴルーチンをキャンセルする
+	isAcquire := s.TryAcquire(1) // 1つのゴルーチンが走ってたら他のゴルーチンはキャンセルさせるような処理となる
+	if !isAcquire {
+		fmt.Println("Could not get lock")
+		return
+	}
+	defer s.Release(1)
+
+	// // Acquireでゴルーチンをロックする
+	// if err := s.Acquire(ctx, 1); err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// defer s.Release(1)
+
+	fmt.Println("Wait...")
+	time.Sleep(1 * time.Second)
+	fmt.Println("Done")
+}
+
+func main() {
+	ctx := context.TODO()
+	go longProcess(ctx)
+	go longProcess(ctx)
+	go longProcess(ctx)
+	time.Sleep(5 * time.Second)
+}
